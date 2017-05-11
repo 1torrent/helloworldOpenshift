9ce3313d7c4ad39431088a6cff7cb8160803b133
p.romagosa.arias <p.romagosa.arias@accenture.com>
2017-05-11 13:27:26 +0200

    Initial version

---
+++ src/main/java/helloworldOpenshift/HelloworldOpenshiftController.java
@@ -0,0 +1,14 @@
+package bootwildfly;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloworldOpenshiftController {
+
+
+    @RequestMapping("hola")
+    public String sayHello(){
+        return ("Hola, SpringBoot on Openshift");
+    }
+}
